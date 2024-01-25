857e71b659fc79ab74ecc2d70b75adcf267a1201
antonio-gg-dev <dev@antonio.gg>
2024-01-25 16:16:08 +0100

    refactor algorithm

--- src/christmas_tree.py
+++ src/christmas_tree.py
@@ -1,17 +1,14 @@
 class ChristmasTree:
 
     def size(self, size: int) -> str:
-        if size == 1:
-            return (
-                "X\n"
-                "|"
-            )
+        tree = ""
 
-        if size == 2:
-            return (
-                " X\n"
-                "XXX\n"
-                " |"
-            )
+        for layer in range(size):
+            tree += (" " * (size - layer - 1))
+            tree += ("X" * (layer * 2 + 1))
+            tree += "\n"
 
-        return '|'
+        tree += (" " * (size - 1))
+        tree += "|"
+
+        return tree
